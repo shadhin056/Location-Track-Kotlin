b71e7719679ede1bf8a09e08191aaf06975b4aed
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-07-16 11:05:39 +0600

    Get lat and lon with manual permission

--- app/src/main/java/com/fundinghelp/locationtrack/MainActivity.kt
+++ app/src/main/java/com/fundinghelp/locationtrack/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.fundinghelp.locationtrack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Address
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.fundinghelp.locationtrack.util.DeviceLocationTracker
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DeviceLocationTracker.DeviceLocationListener{
+
+    private lateinit var deviceLocationTracker: DeviceLocationTracker
+    private var currentlLat:Double = 0.0
+    private var currentLng:Double = 0.0
+    private var Country:String = ""
+    private var cityName:String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        deviceLocationTracker= DeviceLocationTracker(this, this)
+        val toast = Toast.makeText(this, "latitude : "+currentlLat+" longitude : "+currentLng, Toast.LENGTH_LONG)
+    }
+
+    override fun onDeviceLocationChanged(results: List<Address>?) {
+        val currntLocation = results?.get(0);
+        currntLocation?.apply {
+            currentlLat = latitude
+            currentLng = longitude
+            Country = countryCode
+            cityName = getAddressLine(0)
+        }
+        Log.e("XXX","latitude : "+currentlLat+" longitude : "+currentLng)
     }
 }
