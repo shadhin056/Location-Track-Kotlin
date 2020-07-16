cdb7ffd35ed6c6e9836cbf17c9f2b0616860a957
Moniruzzaman ShadhiN <shadhinemail@gmail.com>
2020-07-16 13:06:16 +0600

    Permission lib Added but not loading shows yet

--- app/src/main/java/com/fundinghelp/locationtrack/MainActivity.kt
+++ app/src/main/java/com/fundinghelp/locationtrack/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.fundinghelp.locationtrack
 
+import android.Manifest
 import android.location.Address
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fundinghelp.locationtrack.util.DeviceLocationTracker
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), DeviceLocationTracker.DeviceLocationListener{
 
@@ -19,8 +27,29 @@ class MainActivity : AppCompatActivity(), DeviceLocationTracker.DeviceLocationLi
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         deviceLocationTracker= DeviceLocationTracker(this, this)
+        btnLocation.setOnClickListener {
+            Dexter.withContext(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                        checkLocation();
+                    }
+
+                    override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown( permission: PermissionRequest, token: PermissionToken) {
+                        token.continuePermissionRequest();
+                    }
+                }).check()
+        }
+        }
+
+    private fun checkLocation() {
         val toast = Toast.makeText(this, "latitude : "+currentlLat+" longitude : "+currentLng, Toast.LENGTH_LONG)
+        toast.show()
     }
+
 
     override fun onDeviceLocationChanged(results: List<Address>?) {
         val currntLocation = results?.get(0);
@@ -32,4 +61,5 @@ class MainActivity : AppCompatActivity(), DeviceLocationTracker.DeviceLocationLi
         }
         Log.e("XXX","latitude : "+currentlLat+" longitude : "+currentLng)
     }
+
 }
